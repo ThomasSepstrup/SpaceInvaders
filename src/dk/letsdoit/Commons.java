75f8881ac69284055778d3a68b7d37eaaacb5f4b
Thomas Sepstrup <t.sepstrup@gmail.com>
2017-08-01 13:48:13 +0200

    Initial commit

---
+++ src/dk/letsdoit/Commons.java
@@ -0,0 +1,23 @@
+package dk.letsdoit;
+
+public interface Commons {
+
+    public static final int BOARD_WIDTH = 358;
+    public static final int BOARD_HEIGHT = 350;
+    public static final int GROUND = 290;
+    public static final int BOMB_HEIGHT = 5;
+    public static final int ALIEN_HEIGHT = 12;
+    public static final int ALIEN_WIDTH = 12;
+    public static final int BORDER_RIGHT = 30;
+    public static final int BORDER_LEFT = 5;
+    public static final int GO_DOWN = 15;
+    public static final int NUMBER_OF_ALIENS_TO_DESTROY = 24;
+    public static final int CHANCE = 5;
+    public static final int DELAY = 17;
+    public static final int PLAYER_WIDTH = 15;
+    public static final int PLAYER_HEIGHT = 10;
+    public static final String PLAYER_SHIP_IMAGE = "src/images/player.png";
+    public static final String ALIEN_SHIP_IMAGE = "src/images/alien.png";
+    public static final String SHOT_IMAGE = "src/images/shot.png";
+    public static final String BOMB_IMAGE = "src/images/bomb.png";
+}
